fe8eae7a0a5aecb8092bb07cd058cc836972132f
Yuvaraj S <yuvarajengineer@gmail.com>
2025-11-29 06:41:22 +0530

    Update w dir

--- bastion_host_provisioner.tf
+++ bastion_host_provisioner.tf
@@ -23,7 +23,7 @@ resource "null_resource" "copy_ec2_keys" {
 ## Local Exec Provisioner:  local-exec provisioner (Creation-Time Provisioner - Triggered during Create Resource)
   provisioner "local-exec" {
     command = "echo VPC created on `date` and VPC ID: ${module.vpc.vpc_id} >> creation-time-vpc-id.txt"
-    working_dir = "local-exec-output-files/"
+    working_dir = "./"
   }
 
 }
