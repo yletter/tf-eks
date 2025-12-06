0e3262a9a5efaf8637493c752af7f60669137ae3
Yuvaraj S <yuvarajengineer@gmail.com>
2025-12-06 22:43:03 +0530

    Enable k8s as code for simple v1 app

---
+++ k8s/cluster_setup.tf
@@ -0,0 +1,96 @@
+# Kubernetes Deployment Manifest
+resource "kubernetes_deployment_v1" "release" {
+  metadata {
+    name = "release-blue"
+    labels = {
+      app = "release"
+    }
+  } 
+ 
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "release"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "release"
+        }
+      }
+
+      spec {
+        container {
+          image = "stacksimplify/kubenginx:1.0.0"
+          name  = "release-container"
+          port {
+            container_port = 80
+          }
+          }
+        }
+      }
+    }
+}
+
+# Kubernetes Service Manifest (Type: Load Balancer)
+resource "kubernetes_service_v1" "lb_service" {
+  metadata {
+    name = "release-lb-service"
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment_v1.release.spec.0.selector.0.match_labels.app
+    }
+    port {
+      name        = "http"
+      port        = 80
+      target_port = 80
+    }
+    type = "LoadBalancer"
+  }
+}
+
+# Kubernetes Service Manifest (Type: Node Port Service)
+resource "kubernetes_service_v1" "np_service" {
+  # count = 0
+  metadata {
+    name = "release-nodeport-service"
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment_v1.release.spec.0.selector.0.match_labels.app
+    }
+    port {
+      name        = "http"
+      port        = 80
+      target_port = 80
+      node_port   = 31280
+    }
+    type = "NodePort"
+  }
+}
+
+# Kubernetes Service Manifest (Type: Load Balancer)
+resource "kubernetes_service_v1" "lb_service_nlb" {
+  metadata {
+    name = "release-lb-service-nlb"
+    annotations = {
+      "service.beta.kubernetes.io/aws-load-balancer-type" = "nlb"    # To create Network Load Balancer  
+    }   
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment_v1.release.spec.0.selector.0.match_labels.app
+    }
+    port {
+      name        = "http"
+      port        = 80
+      target_port = 80
+    }
+    type = "LoadBalancer"
+  }
+}
