1e14c2de90e4c768877ccc0bd7ea0ec1c1cf6876
Yuvaraj S <yuvarajengineer@gmail.com>
2025-12-07 19:32:40 +0530

    Add oidc resources for k8s

--- eks.tf
+++ eks.tf
@@ -127,3 +127,34 @@ resource "aws_eks_node_group" "eks_ng_public" {
     Name = "Public-Node-Group"
   }
 }
+
+data "aws_partition" "current" {}
+
+resource "aws_iam_openid_connect_provider" "oidc_provider" {
+  client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
+  thumbprint_list = "9e99a48a9960b14926bb7f3b02e22da2b0ab7280"
+  url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+
+  tags = merge(
+    {
+      Name = "release-cluster-eks-irsa"
+    }
+  )
+}
+
+# Output: AWS IAM Open ID Connect Provider ARN
+output "aws_iam_openid_connect_provider_arn" {
+  description = "AWS IAM Open ID Connect Provider ARN"
+  value = aws_iam_openid_connect_provider.oidc_provider.arn 
+}
+
+# Extract OIDC Provider from OIDC Provider ARN
+locals {
+    aws_iam_oidc_connect_provider_extract_from_arn = element(split("oidc-provider/", "${aws_iam_openid_connect_provider.oidc_provider.arn}"), 1)
+}
+
+# Output: AWS IAM Open ID Connect Provider
+output "aws_iam_openid_connect_provider_extract_from_arn" {
+  description = "AWS IAM Open ID Connect Provider extract from ARN"
+   value = local.aws_iam_oidc_connect_provider_extract_from_arn
+}
