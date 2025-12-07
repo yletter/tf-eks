9de1d02f88166871e6a1e83c8d885c7930d636c2
Yuvaraj S <yuvarajengineer@gmail.com>
2025-12-07 19:34:42 +0530

    Thumb print list

--- eks.tf
+++ eks.tf
@@ -132,7 +132,7 @@ data "aws_partition" "current" {}
 
 resource "aws_iam_openid_connect_provider" "oidc_provider" {
   client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
-  thumbprint_list = "9e99a48a9960b14926bb7f3b02e22da2b0ab7280"
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
   url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 
   tags = merge(
