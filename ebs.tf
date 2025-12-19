0a1dd5d38492aa9a6aa52d9f2fdc4c832d7ab114
Yuvaraj S <yuvarajengineer@gmail.com>
2025-12-19 12:08:37 +0530

    Install EBS CSI

---
+++ ebs.tf
@@ -0,0 +1,98 @@
+# Datasource: EBS CSI IAM Policy get from EBS GIT Repo (latest)
+data "http" "ebs_csi_iam_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-ebs-csi-driver/master/docs/example-iam-policy.json"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
+}
+
+output "ebs_csi_iam_policy" {
+  value = data.http.ebs_csi_iam_policy.response_body
+}
+
+# Resource: Create EBS CSI IAM Policy 
+resource "aws_iam_policy" "ebs_csi_iam_policy" {
+  name        = "release-AmazonEKS_EBS_CSI_Driver_Policy"
+  path        = "/"
+  description = "EBS CSI IAM Policy"
+  policy      = data.http.ebs_csi_iam_policy.response_body
+}
+
+output "ebs_csi_iam_policy_arn" {
+  value = aws_iam_policy.ebs_csi_iam_policy.arn 
+}
+
+# Resource: Create IAM Role and associate the EBS IAM Policy to it
+resource "aws_iam_role" "ebs_csi_iam_role" {
+  name = "release-ebs-csi-iam-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Federated = "${aws_iam_openid_connect_provider.oidc_provider.arn}"
+        }
+        Condition = {
+          StringEquals = {            
+            "${local.aws_iam_oidc_connect_provider_extract_from_arn}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "release-ebs-csi-iam-role"
+  }
+}
+
+# Associate EBS CSI IAM Policy to EBS CSI IAM Role
+resource "aws_iam_role_policy_attachment" "ebs_csi_iam_role_policy_attach" {
+  policy_arn = aws_iam_policy.ebs_csi_iam_policy.arn 
+  role       = aws_iam_role.ebs_csi_iam_role.name
+}
+
+output "ebs_csi_iam_role_arn" {
+  description = "EBS CSI IAM Role ARN"
+  value = aws_iam_role.ebs_csi_iam_role.arn
+}
+
+# Datasource: EKS Cluster Auth 
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+
+# Install EBS CSI Driver using HELM
+# Resource: Helm Release 
+resource "helm_release" "ebs_csi_driver" {
+  depends_on = [aws_iam_role.ebs_csi_iam_role]
+  name       = "release-aws-ebs-csi-driver"
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  chart      = "aws-ebs-csi-driver"
+  namespace = "kube-system"     
+
+  set = [
+    {
+    name = "image.repository"
+    value = "602401143452.dkr.ecr.us-east-1.amazonaws.com/eks/aws-ebs-csi-driver" # Changes based on Region - This is for us-east-1 Additional Reference: https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html
+    },
+    {
+    name  = "controller.serviceAccount.create"
+    value = "true"
+    },
+    {
+    name  = "controller.serviceAccount.name"
+    value = "ebs-csi-controller-sa"
+    },
+    {
+    name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = "${aws_iam_role.ebs_csi_iam_role.arn}"
+    }
+  ]
+}
--- main.tf
+++ main.tf
@@ -6,12 +6,42 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.21.0"
     }
+
+    helm = {
+      source = "hashicorp/helm"
+      version = "~> 3.1.1"
+    }
+
+    http = {
+      source = "hashicorp/http"
+      version = "~> 3.5.0"
+    }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = ">= 3.0.0"
+    }
   }
 }
 
 # Provider Block
 provider "aws" {
   region  = var.aws_region
+}
+
+# Terraform HTTP Provider Block
+provider "http" {
+  # Configuration options
+}
+
+# HELM Provider
+provider "helm" {
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 terraform {
